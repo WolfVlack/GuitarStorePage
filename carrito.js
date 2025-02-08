ffafd81d6be5bf446beaec721ae9c3d328be47d5
WolfVlack <106246304+WolfVlack@users.noreply.github.com>
2025-02-08 21:08:57 +0100

    Add files via upload

--- carrito.js
+++ carrito.js
@@ -5,28 +5,32 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeCartBtn = document.getElementById('closeCartBtn');
     const checkoutBtn = document.getElementById('checkoutBtn');
     const cartItemsContainer = document.getElementById('cartItems');
+    const totalAmount = document.getElementById('totalAmount');
 
     let cart = [];
 
     // Función para actualizar el carrito en la ventana
     function updateCartUI() {
         cartItemsContainer.innerHTML = ''; // Limpiar el contenedor
+        let total = 0;
         cart.forEach((item, index) => {
             const itemDiv = document.createElement('div');
             itemDiv.classList.add('cart-item');
             itemDiv.innerHTML = `
-                <img src="${item.img}" alt="${item.name}" style="width: 50px;">
-                <span>${item.name}</span>
-                <span>$${item.price}</span>
+                <img class="cartProductImage" src="${item.img}" alt="${item.name}">
+                <span class="cartProductName">${item.name}</span>
+                <span class="cartProductPrice">$${item.price}</span>
                 <div class="quantity-controls">
-                    <button onclick="changeQuantity(${index}, -1)">-</button>
+                    <button class="decreaseBTN" onclick="changeQuantity(${index}, -1)">-</button>
                     <span>${item.quantity}</span>
-                    <button onclick="changeQuantity(${index}, 1)">+</button>
+                    <button class="increaseBTN" onclick="changeQuantity(${index}, 1)">+</button>
                 </div>
-                <button onclick="removeItem(${index})">Eliminar</button>
+                <button class="removeBTN" onclick="removeItem(${index})">X</button>
             `;
             cartItemsContainer.appendChild(itemDiv);
+            total += item.price * item.quantity;
         });
+        totalAmount.textContent = total.toFixed(2);
     }
 
     // Mostrar el carrito con animación
